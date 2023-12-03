b6598e85470d81e65d9612743190ffee509f19d0
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-03 11:09:24 +0300

    add app products

--- store/store/settings.py
+++ store/store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=j7jm(o=$h+$-u$^@yys&+r^dl+98&y9rj%s0(i&xtom9%4)1u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products',
 ]
 
 MIDDLEWARE = [
