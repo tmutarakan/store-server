f286345fd8d43ed99f768c7e4d8bbc48a9813859
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-04 21:17:02 +0300

    add models to admin panel

--- store/products/models.py
+++ store/products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     descrtion = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,3 +16,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Продукт: {self.name} | Категория: {self.category.name}"
