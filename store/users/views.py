3d4838830e278e2fd399637e60cf4563b0a2a8cb
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-07 21:17:11 +0300

    add user registration form

--- store/users/views.py
+++ store/users/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -16,11 +15,17 @@ def login(request):
             if user:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
+    form = UserLoginForm()
     context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
