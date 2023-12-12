3622261d56c46be5eafcd8fcf85de90bb684daa1
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-12 20:14:47 +0300

    add pagination

--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from products.models import Product, ProductCategory, Basket
 
@@ -9,10 +10,14 @@ def index(request):
     return render(request, "products/index.html", context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
     context = {
         "title": "Store",
-        "products": Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),
+        "products": products_paginator,
         "categories": ProductCategory.objects.all(),
     }
     return render(request, "products/products.html", context)
