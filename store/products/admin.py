4f57bef3f754fee6f7f6d9c11c87d708625c84c6
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-19 11:02:20 +0300

    edit django admin

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
