f8f5d474c29ac52895dd8718c9a9e6b185aa5acc
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-20 15:46:49 +0300

    add context_processors

--- store/users/views.py
+++ store/users/views.py
@@ -10,50 +10,47 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 from users.models import User, EmailVerification
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from products.models import Basket
 from common.views import TitleMixin
 
 
 class UserLoginView(TitleMixin, LoginView):
-    template_name = 'users/login.html'
+    template_name = "users/login.html"
     form_class = UserLoginForm
-    title = 'Store - Авторизация'
+    title = "Store - Авторизация"
 
 
 class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
-    template_name = 'users/register.html'
-    success_url = reverse_lazy('users:login')
-    success_message = 'Поздравляем! Вы успешно зарегистрированы!'
-    title = 'Store - Регистрация'
+    template_name = "users/register.html"
+    success_url = reverse_lazy("users:login")
+    success_message = "Поздравляем! Вы успешно зарегистрированы!"
+    title = "Store - Регистрация"
 
 
 class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
-    template_name = 'users/profile.html'
-    title = 'Store - Личный кабинет'
+    template_name = "users/profile.html"
+    title = "Store - Личный кабинет"
 
     def get_success_url(self) -> str:
-        return reverse_lazy('users:profile', args=(self.object.id,))
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['baskets'] = Basket.objects.filter(user=self.request.user)
-        return context
+        return reverse_lazy("users:profile", args=(self.object.id,))
 
 
 class EmailVerificationView(TitleMixin, TemplateView):
-    title = 'Store - Подтверждение электронной почты'
-    template_name = 'users/email_verification.html'
+    title = "Store - Подтверждение электронной почты"
+    template_name = "users/email_verification.html"
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        code = kwargs['code']
-        user = User.objects.get(email=kwargs['email'])
+        code = kwargs["code"]
+        user = User.objects.get(email=kwargs["email"])
         email_verifications = EmailVerification.objects.filter(user=user, code=code)
-        if email_verifications.exists() and not email_verifications.first().is_expired():
+        if (
+            email_verifications.exists()
+            and not email_verifications.first().is_expired()
+        ):
             user.is_verified_email = True
             user.save()
             return super().get(request, *args, **kwargs)
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse("index"))
