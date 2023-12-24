88fafdcc0758dbcc6e78a534175c9dc8abe39195
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-24 13:37:47 +0300

    add redis for cache

--- store/store/settings.py
+++ store/store/settings.py
@@ -102,6 +102,13 @@ INTERNAL_IPS = [
 ]
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
