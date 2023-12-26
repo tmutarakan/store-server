126cc93ea84c80de75b33ea1c69ebd52e658698e
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-26 07:29:47 +0300

    add  OrderModel and OrderForm

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,6 +1,8 @@
-from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
+from orders.forms import OrderForm
 
-class OrderCreateView(TemplateView):
+
+class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
