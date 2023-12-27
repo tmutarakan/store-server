fabb962c03a31d0b69b103d1a07cf9260c0697c2
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2023-12-27 23:24:38 +0300

    add connecting yookassa

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from orders.views import OrderCreateView
+from orders.views import OrderCreateView, SuccesTemplateView, CanceledTemplateView
 
 
 app_name = 'orders'
@@ -8,4 +8,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
+    path('order-success/', SuccesTemplateView.as_view(), name='order_success'),
+    path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
--- store/orders/views.py
+++ store/orders/views.py
@@ -1,10 +1,31 @@
+from typing import Any
+from uuid import uuid4
+from http import HTTPStatus
+
+from yookassa import Configuration, Payment
+
+from django.conf import settings
 from django.forms.models import BaseModelForm
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+
+
+Configuration.account_id = settings.YOOKASSA_ACCOUNT_ID
+Configuration.secret_key = settings.YOOKASSA_SECRET_KEY
+
+
+class SuccesTemplateView(TitleMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Store - Спасибо за заказ!'
+
+
+class CanceledTemplateView(TitleMixin, TemplateView):
+    template_name = 'orders/canceled.html'
 
 
 class OrderCreateView(TitleMixin, CreateView):
@@ -12,6 +33,22 @@ class OrderCreateView(TitleMixin, CreateView):
     form_class = OrderForm
     success_url = reverse_lazy('orders:order_create')
     title = 'Store - Оформление заказа'
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        super().post(request, *args, **kwargs)
+        payment = Payment.create({
+            "amount": {
+                "value": "100.00",
+                "currency": "RUB"
+            },
+            "confirmation": {
+                "type": "redirect",
+                "return_url": f"{settings.DOMAIN_NAME}{reverse('orders:order_success')}"
+            },
+            "capture": True,
+            "description": "Заказ №1"
+        }, uuid4())
+        return HttpResponseRedirect(payment.confirmation.confirmation_url, status=HTTPStatus.SEE_OTHER)
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         form.instance.initiator = self.request.user
