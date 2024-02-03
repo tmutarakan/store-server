d0368cf3d43dcf4aed10281b67f343e6e0aad659
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2024-02-03 07:29:18 +0300

    edit settings for static files

--- store/store/settings.py
+++ store/store/settings.py
@@ -184,7 +184,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / 'static',)
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
